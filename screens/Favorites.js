db4f3b779171e17b5b2c5a997a585fc0c03254fa
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-12-03 23:46:05 +0000

    Displaying favorites meals.

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,12 +1,43 @@
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, FlatList } from 'react-native';
+import { FavoritesContext } from '../store/context/favorites-context';
+import { useContext } from 'react';
+import Meal from '../components/meals/Meal';
+import { MEALS } from '../data/dummy-data';
 
 const Favorites = () => {
+  const favoriteMealsContext = useContext(FavoritesContext)
+  const displayedMeals = []
+
+  for(let i = 0; i < favoriteMealsContext.ids.length; i++){
+    for(let j = 0; j < MEALS.length; j++){
+      if(favoriteMealsContext.ids[i] == MEALS[j].id){
+        displayedMeals.push(MEALS[j])
+      }
+    }
+  }
 
   return(
     <>
-      <Text>Favorites</Text>
+      <Text style={styles.title}>Favorites</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(meal) => meal.id}
+        renderItem={({item}) => {
+          return <Meal meal={item} />
+        }}
+      />
     </>
   )
 }
 
 export default Favorites;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    marginTop: 15,
+    marginBottom: 25,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
